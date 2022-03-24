1a221ba36de7d7c75d0d3720d2da3e33c4b6fd61
Zeonixas <zeoniksas@gmail.com>
2022-03-24 23:52:36 +0200

    Add home page buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,38 @@
-import {
-    Badge,
-    chakra,
-    Code,
-    Heading,
-    List,
-    ListItem,
-    OrderedList,
-  } from '@chakra-ui/react'
-  import React from 'react'
-  import { Link, useLocation } from 'react-router-dom'
-  import { Layout } from '../components/Layout'
-  import { useAuth } from '../contexts/AuthContext'
-  
-  export default function Homepage() {
+import {Grid, GridItem, Heading,} from '@chakra-ui/react'
+import React from 'react'
+import {Layout} from '../components/Layout'
+import Navlink from "../components/Navlink";
+import {useAuth} from "../contexts/AuthContext";
+
+export default function Homepage() {
+    const {logout, currentUser } = useAuth()
+
     return (
-      <Layout>
-        <Heading>Home page</Heading>
-      </Layout>
+        <Layout>
+            <Heading>Home page</Heading>
+            {currentUser && (
+                <Grid gap={6}>
+                    <GridItem w='100%' h='10' align='center'>
+                        <Navlink  to='/admin' name='Admin panel'/>
+                    </GridItem>
+                    <GridItem w='100%' h='10' align='center'>
+                        <Navlink to='/qrscanner' name='QR scanner'/>
+                    </GridItem>
+                    <GridItem w='100%' h='10' align='center'>
+                        <Navlink to='/qrgenerator' name='QR generator'/>
+                    </GridItem>
+                    <GridItem w='100%' h='10' align='center'>
+                        <Navlink
+                            to='/logout'
+                            name='Logout'
+                            onClick={async e => {
+                                e.preventDefault()
+                                await logout()
+                            }}
+                        />
+                    </GridItem>
+                </Grid>
+            )}
+        </Layout>
     )
-  }
+}
