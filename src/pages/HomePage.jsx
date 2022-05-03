2d8dcea7402d08053eeefd58f3eb6c17661c6bea
Redas <redass95@outlook.com>
2022-05-03 19:30:45 +0300

    small fix #3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,12 +14,12 @@ export default function Homepage() {
                 <Grid gap={6}>
                     {userRole === 1 && (
                         <GridItem w='100%' h='10' align='center'>
-                            <Navlink  to='/student' name='Student page' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
+                            <Navlink  to='/student' name='Student Panel' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
                         </GridItem>
                     )}
                     {userRole === 2 && (
                         <GridItem w='100%' h='10' align='center'>
-                            <Navlink  to='/lecturer' name='Lecturer page' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
+                            <Navlink  to='/lecturer' name='Lecturer Panel' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
                         </GridItem>
                     )}
                     {userRole === 3 && (
@@ -30,10 +30,10 @@ export default function Homepage() {
 
 
                     <GridItem w='100%' h='10' align='center'>
-                        <Navlink to='/qrscanner' name='QR scanner' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
+                        <Navlink to='/qrscanner' name='QR Scanner' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
                     </GridItem>
                     <GridItem w='100%' h='10' align='center'>
-                        <Navlink to='/qrgenerator' name='QR generator' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
+                        <Navlink to='/qrgenerator' name='QR Generator' colorScheme='teal' size='lg' width="350px" variant ='outline'/>
                     </GridItem>
                 </Grid>
             )}
