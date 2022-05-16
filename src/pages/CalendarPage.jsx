4fbf21a18a7e7f4bc539707d988bca0ded75b5d7
arnnau <arnas.naujalis@ktu.edu>
2022-05-16 21:47:05 +0300

    Changed calendar package to a working one

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,35 +1,153 @@
-import React from 'react';
-import Dayz from 'dayz';
-// could also import the sass if you have a loader at dayz/dayz.scss
-import "dayz/dist/dayz.css";
-import moment from 'moment';
+import React from 'react'
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import { Navbar } from '../components/Navbar'
-import { Box } from '@chakra-ui/react'
 
-const date = moment('2022-05-16');
+let eventGuid = 0
+let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
-// would come from a network request in a "real" app
-const EVENTS = new Dayz.EventsCollection([
-    { content: 'A short event',
-      range: moment.range( date.clone(),
-                           date.clone().add(1, 'day') ) },
-    { content: 'Two Hours ~ 8-10',
-      range: moment.range( date.clone().hour(8),
-                           date.clone().hour(10) ) },
-    { content: "A Longer Event",
-      range: moment.range( date.clone().subtract(2,'days'),
-                           date.clone().add(8,'days') ) }
-]);
+const INITIAL_EVENTS = [
+    {
+      id: createEventId(),
+      title: 'All-day event',
+      start: todayStr
+    },
+    {
+      id: createEventId(),
+      title: 'Timed event',
+      start: todayStr + 'T12:00:00'
+    }
+  ]
+  
+  function createEventId() {
+    return String(eventGuid++)
+  }
 
-export default function CalendarPage() {
+export default class CalendarPage extends React.Component {
+
+  state = {
+    weekendsVisible: true,
+    currentEvents: []
+  }
+
+  render() {
     return (
-    <Box>
-        <Navbar/>
-        <Dayz
-            display='week'
-            date={date}
-            events={EVENTS}
-        />
-    </Box>
+        
+      <div className='demo-app'>
+          <Navbar/>
+        {this.renderSidebar()}
+        <div className='demo-app-main'>
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            initialView='dayGridMonth'
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={this.state.weekendsVisible}
+            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            select={this.handleDateSelect}
+            eventContent={renderEventContent} // custom render function
+            eventClick={this.handleEventClick}
+            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+            /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+          />
+        </div>
+      </div>
     )
+  }
+
+  renderSidebar() {
+    return (
+      <div className='demo-app-sidebar'>
+        <div className='demo-app-sidebar-section'>
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className='demo-app-sidebar-section'>
+          <label>
+            <input
+              type='checkbox'
+              checked={this.state.weekendsVisible}
+              onChange={this.handleWeekendsToggle}
+            ></input>
+            toggle weekends
+          </label>
+        </div>
+        <div className='demo-app-sidebar-section'>
+          <h2>All Events ({this.state.currentEvents.length})</h2>
+          <ul>
+            {this.state.currentEvents.map(renderSidebarEvent)}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+  handleWeekendsToggle = () => {
+    this.setState({
+      weekendsVisible: !this.state.weekendsVisible
+    })
+  }
+
+  handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
+    }
+  }
+
+  handleEventClick = (clickInfo) => {
+      clickInfo.event.remove()
+  }
+
+  handleEvents = (events) => {
+    this.setState({
+      currentEvents: events
+    })
+  }
+
+}
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
+}
+
+function renderSidebarEvent(event) {
+  return (
+    <li key={event.id}>
+      <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
+      <i>{event.title}</i>
+    </li>
+  )
 }
