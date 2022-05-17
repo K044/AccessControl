98c7adee19c25c167a1901f9d238bcf2987c1059
arnnau <arnas.naujalis@ktu.edu>
2022-05-17 22:19:35 +0300

    Gitignore changes, firebase tools

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -11,13 +11,9 @@ let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of tod
 const INITIAL_EVENTS = [
     {
       id: createEventId(),
-      title: 'All-day event',
-      start: todayStr
-    },
-    {
-      id: createEventId(),
-      title: 'Timed event',
-      start: todayStr + 'T12:00:00'
+      title: '102. XI',
+      start: todayStr + 'T12:00:00',
+      end: todayStr + 'T13:30:00'
     }
   ]
   
